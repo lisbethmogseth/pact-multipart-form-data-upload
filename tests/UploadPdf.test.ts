470ebc38ec5704553ced1df138d123aa3bb36162
Lisbeth Mogseth <lisbeth.mogseth@regionstockholm.se>
2023-07-21 13:22:38 +0200

    Adding a test showing the behavior without the expected header

--- tests/UploadPdf.test.ts
+++ tests/UploadPdf.test.ts
@@ -14,32 +14,65 @@ describe("POST /api/upload", () => {
   const PDF_FILE_PATH = path.join(process.cwd(), "tests/empty.pdf");
   const PDF_FILE_DATA = readFileSync(PDF_FILE_PATH);
 
-  it("upload file and return fileId", async () => {
-    provider
-      .uponReceiving("a request to upload pdf")
-      .withRequestMultipartFileUpload(
-        {
-          method: "POST",
-          path: `/api/upload`,
-        },
-        "text/plain",
-        PDF_FILE_PATH,
-        "file"
-      )
-      .willRespondWith({
-        headers: { "Content-Type": "text/plain;charset=utf-8" },
-        status: 200,
-        body: MatchersV3.string("some string"),
-      });
+  describe("Specifying expected response headers causes issue with the body", () => {
+    it("upload file and return fileId as string", async () => {
+      provider
+        .uponReceiving("a request to upload pdf")
+        .withRequestMultipartFileUpload(
+          {
+            method: "POST",
+            path: `/api/upload`,
+          },
+          "text/plain",
+          PDF_FILE_PATH,
+          "file"
+        )
+        .willRespondWith({
+          headers: { "Content-Type": "text/plain;charset=utf-8" },
+          status: 200,
+          body: MatchersV3.string("some string"),
+        });
 
-    await provider.executeTest(async (mockServer) => {
-      const result = await uploadService.uploadPdf({
-        baseUrl: mockServer.url,
-        file: new Blob([PDF_FILE_DATA], {
-          type: "application/pdf",
-        }),
+      await provider.executeTest(async (mockServer) => {
+        const result = await uploadService.uploadPdf({
+          baseUrl: mockServer.url,
+          file: new Blob([PDF_FILE_DATA], {
+            type: "application/pdf",
+          }),
+        });
+        expect(result.data).toEqual("some string");
       });
-      // expect(result.data).toEqual("some string");
+    });
+  });
+
+  describe("Without expected response headers the body is as expected", () => {
+    it("upload file and return fileId as string", async () => {
+      provider
+        .uponReceiving("a request to upload pdf")
+        .withRequestMultipartFileUpload(
+          {
+            method: "POST",
+            path: `/api/upload`,
+          },
+          "text/plain",
+          PDF_FILE_PATH,
+          "file"
+        )
+        .willRespondWith({
+          // headers: { "Content-Type": "text/plain;charset=utf-8" },
+          status: 200,
+          body: MatchersV3.string("some string"),
+        });
+
+      await provider.executeTest(async (mockServer) => {
+        const result = await uploadService.uploadPdf({
+          baseUrl: mockServer.url,
+          file: new Blob([PDF_FILE_DATA], {
+            type: "application/pdf",
+          }),
+        });
+        expect(result.data).toEqual("some string");
+      });
     });
   });
 });
